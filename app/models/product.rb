cdfbad376ec752f98f9d639472d32939f8b5cf4b
qop <alex7orl@gmail.com>
2018-05-28 09:20:25 -0700

    test+vali

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,10 @@ validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
 validates :image_url, allow_blank: true, format: 	{ with: %r{\.(gif|jpg|jpeg|png)\Z}i,
 													message: 'URL accepts JPG JPEG PNG GIF'}
-end
+
+#additional task from the end of the book's chapter: 
+#total length of the title should not be more than 10 symbols
+
+validates :title, length: { maximum: 10 } 
+
+end 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,74 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	fixtures :products
+
+  #here are some methods, needed for tests below
+
+  #method for test "image url"
+  def new_product(image_url)
+  	
+  	Product.new(title: "r", description: "r", price: 1, image_url: image_url)
+  
+  end
+
+	# here are the tests
+
+	test "image url" do
+	
+	ok = %w{fred.gif fred.jpg fred.png FRED.JPG fRed.Jpg http://a.b.c/x/w/q/fred.gif }
+
+	bad = %w{ fred.dc fred.gif/123 fred.gif.php}
+
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} should not be invalid"
+		end
+
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} should not be valid"
+		end
+
+	end
+
+	test "product attributes must not be empty" do 
+  	
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:price].any?
+  	assert product.errors[:image_url].any?
+  
+  end
+
+  test "product price must be positive" do 
+  	
+  	product = Product.new(title: "Book Title", description: "yyy", image_url: "123.jpg")
+
+  	#specially made failure
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal ["must be greater then or equal to 0.01"],
+  	product.errors[:price]
+  	
+  	product.price = 0
+  	assert product.invalid?
+
+  	assert_equal ["must be greater then or equal to 0.01"],
+  	product.price = 1
+  	assert product.valid?
+
+  end
+  
+  test "product is not valid if it has not unique title" do
+  	
+  	product = Product.new(title: products(:ruby).title, description: "yyy", image_url: "123.jpg")
+  	assert product.invalid?
+  	assert_equal ["has already been taken"], product.errors[:title]
+
+  end
+
+
+
 end
