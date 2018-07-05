211bb337ec62e5a3bfe00dcdb1640be3e08c215b
qop <alex7orl@gmail.com>
2018-07-05 10:21:07 -0700

    12.1 bug fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ActiveRecord::Base
 	
 	PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
 	
-	validates :name, :email, presence: true
+	validates :name, :address, :email, presence: true
 	validates :pay_type, inclusion: PAYMENT_TYPES 
 
 	def add_line_items_from_cart(cart)
